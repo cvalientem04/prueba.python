bca868061824e0c0dd2191c7d271fae2500392b1
Carlos Valiente Marin <cvalientem04@educarex.es>
2026-02-11 10:29:20 +0000

    guardar cambios

---
+++ function.py
@@ -0,0 +1,14 @@
+def saludo():
+    print("¡Hola! Bienvenido a Python.")
+
+
+
+
+saludo()
+
+def potencia(numero):
+    resultado = numero ** 2
+    print(f"El resultado de elevar {numero} al cuadrado es: {resultado}")
+
+
+potencia(5)
